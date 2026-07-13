ccbe4104dd4fb122b3891c39b72b2462257c26d1
Sumalatha R Naik <suma03.venu20@gmail.com>
2026-07-13 22:52:33 +0530

    Day 4: Added React state and interactive dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+
+
 import "../styles/Dashboard.css";
 
 function Dashboard() {
+const [code, setCode] = useState("");
+const [reviewResult, setReviewResult] = useState("");
+ const handleReview = () => {
+
+  if (code.trim() === "") {
+    alert("Please enter some code first!");
+    return;
+  }
+
+ setReviewResult(`
+✅ Code received successfully!
+
+📄 Total characters: ${code.length}
+
+🤖 AI review will be displayed here after backend integration.
+`);
+
+};
+
   return (
     <div className="dashboard">
 
@@ -9,10 +31,13 @@ function Dashboard() {
       <p>Welcome to AI Code Review Assistant</p>
 
       <textarea
-        placeholder="Paste your code here..."
-      ></textarea>
+  placeholder="Paste your code here..."
+  value={code}
+  onChange={(e) => setCode(e.target.value)}
+></textarea>
 
-      <button className="review-btn">
+      <button className="review-btn"  onClick={handleReview}>
+         
         Review Code
       </button>
 
@@ -21,8 +46,8 @@ function Dashboard() {
         <h2>Review Results</h2>
 
         <p>
-          Your AI review results will appear here.
-        </p>
+  {reviewResult || "Your AI review results will appear here."}
+</p>
 
       </div>
 
